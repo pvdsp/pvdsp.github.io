2d45edf921e0f8faf04484bc438e759d29d598b3
Patrick Van der Spiegel <patrick.vanderspiegel@gmail.com>
2016-07-20 21:32:17 +0200

    Code clean-up + Extra content

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,13 @@
-var openMenuLogo = function () {
-  $('#logo').click(function () {
-    $('.menu').fadeToggle(1000).removeClass('hidden');
-  })
+var openMenuLogo = function() {
+    $('#logo').click(function() {
+        $('.menu').fadeToggle(1000).removeClass('hidden');
+    })
 };
 
-var openMenuKey = function () {
-  $(document).keypress(function () {
-    $('.menu').fadeToggle(1000).removeClass('hidden');
-  })
+var openMenuKey = function() {
+    $(document).keypress(function() {
+        $('.menu').fadeToggle(1000).removeClass('hidden');
+    })
 };
 
 $(document).ready(openMenuLogo);
