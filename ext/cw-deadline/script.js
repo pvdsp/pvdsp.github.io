0414200d3acae1b5efa3ae9bb15b4678cabd519c
Thibault Hendrieckx <thibault.hendrieckx@vub.be>
2018-02-15 14:51:58 +0100

    Changed datastructure and page init

--- ext/cw-deadline/script.js
+++ ext/cw-deadline/script.js
@@ -1,31 +1,46 @@
-var now = new Date();
-var dl_OO = new Date("November 28, 2016 00:00:00");
-var dl_Cs = new Date("January 9, 2017 00:00:00");
-var dl_Algo2_1 = new Date("February 12, 2017 08:00:00");
-var dl_Scp2_2 = new Date("December 16, 2016 16:00:00");
-var dl_pp_d1 = new Date("January 17, 2017 00:00:00");
-var dl_pp_d2 = new Date("May 30, 2017 00:00:00");
-var dl_pp_tv = new Date("March 28, 2017 00:00:00");
-var dl_OO_scala = new Date("January 10, 2017 00:00:00");
-var dl_Scp2_snake = new Date("January 16, 2017 16:00:00");
-var dl_Algo2_2 = new Date("June 9, 2017 23:59:00");
+// Loading and compiling the template
+var source = document.getElementById("entry-template").innerHTML;
+var template = Handlebars.compile(source);
 
-function postData(deadline, id) {
-    if (now < deadline) {
-        document.getElementById(id).innerHTML = countdown(null, deadline);
+// A basic deadline object
+function Deadline(title, date) {
+    this.title = title;
+    this.date = new Date(date);
+}
+
+// A list containing all the deadlines, add deadlines here
+var deadlines = [
+    new Deadline("Computersystemen: Project", "January 9, 2017 00:00:00"),
+    new Deadline("Algoritmen & Datastructuren II: Taak 1\t", "February 12, 2017 08:00:00"),
+    new Deadline("SCP2: Taak 2", "December 16, 2016 16:00:00"),
+    new Deadline("Programmeerproject: Deel 1", "January 17, 2017 00:00:00"),
+    new Deadline("Programmeerproject: Deel 2\t", "May 30, 2017 00:00:00"),
+    new Deadline("Programmeerproject: Verslag", "March 28, 2017 00:00:00"),
+    new Deadline("Object Oriented: Project Scala", "January 10, 2017 00:00:00"),
+    new Deadline("SCP2: Snake", "January 16, 2017 16:00:00"),
+    new Deadline("Algoritmen & Datastructuren II: Taak 2", "June 9, 2017 23:59:00")
+];
+
+// The countdown update callback
+function postData(deadline) {
+    if (new Date() < deadline.date) {
+        document.getElementById(safeId(deadline.title)).innerHTML = countdown(null, deadline.date);
     } else {
-        document.getElementById(id).innerHTML = "Deadline passed"
+        document.getElementById(safeId(deadline.title)).innerHTML = "Deadline passed"
     }
-};
+}
 
-setInterval(function() {
-    postData(dl_Cs, "deadline-cs");
-    postData(dl_Algo2_1, "deadline-algo-taak1");
-    postData(dl_Scp2_2, "deadline-scp2-t2");
-    postData(dl_pp_d1, "deadline-pp-d1");
-    postData(dl_pp_d2, "deadline-pp-d2");
-    postData(dl_pp_tv, "deadline-pp-tv");
-    postData(dl_OO_scala, "deadline-OO-scala");
-    postData(dl_Scp2_snake, "deadline-scp2-snake");
-    postData(dl_Algo2_2, "deadline-algo-taak2");
-}, 1000);
+// Initialising the page
+function init() {
+    for (var i = 0; i < deadlines.length; i++) {
+        var deadline = deadlines[i];
+        document.getElementById("deadlines").innerHTML += template({title: deadline.title, date: deadline.date});
+    }
+    setInterval(function () {
+        deadlines.forEach(function (deadline) {
+            postData(deadline)
+        })
+    }, 1000)
+}
+
+init();
